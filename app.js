42d0fddca138ede8aae8f55d9418b2d1fc452e8f
tarun <tarunyadav83333@gmail.com>
2021-11-11 20:52:48 +0530

    done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {fork} = require('child_process');
-
+const os = require('os');
 const fetch_details = require('./fetch_details')
 const mcache = require('memory-cache');
+const cluster = require('cluster');
 
+const cpu = 4
 
 const app = express();
 const port = process.env.PORT||3000
@@ -11,6 +12,7 @@ const port = process.env.PORT||3000
 const url_leetcode = 'https://leetcode.com/';
 const url_codeforces = 'https://codeforces.com/profile/';
 const url_codechef = 'https://www.codechef.com/users/';
+
 
 app.use(function (req, res, next) {
     let key = '__express__'+req.url;
@@ -28,25 +30,29 @@ app.use(function (req, res, next) {
     }
     next()
   })
-
+  
 app.get('/', (req, res) => {
     let cdchf = req.query.cdchf;
     let cdf = req.query.cdf;
     let leet = req.query.leet
-    const forked = fork('fetch_details.js');
     fetch_details(url_codechef+cdchf,url_leetcode+leet,url_codeforces+cdf).then((details)=>{
-        res.send(details)
-    })
-    // forked.on('message',(msg)=>{
-    //     console.log(msg);
-    //     res.send(msg);
-    //     // process.exit();
-    // })
+        res.send(details);
+        console.log(`${process.pid}`)
 
-    // forked.send({url_codechef:url_codechef+cdchf,url_leetcode:url_leetcode+leet,url_codeforces:url_codeforces+cdf});
-    
+    })
+
 })
-  
-app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-})
+
+
+
+
+if(cluster.isMaster){
+    for(let i = 0;i<cpu;i++){
+        cluster.fork();
+    }
+}
+else{
+    app.listen(port, () => {
+        console.log(`app listening at ${process.pid} http://localhost:${port}`)
+    })
+}
