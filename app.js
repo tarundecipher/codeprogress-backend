35abcbce1b0e674b1c50445cc0912ab9b8c36cbc
tarun <tarunyadav83333@gmail.com>
2021-11-25 20:49:59 +0530

    added nginx as loadbalancer

--- app.js
+++ app.js
@@ -1,60 +1,70 @@
-const express = require('express');
-const os = require('os');
-const fetch_details = require('./fetch_details')
-const mcache = require('memory-cache');
-const cluster = require('cluster');
-const redis = require('redis');
-
-const cpu = 4
+const express = require("express");
+const fetch_details = require("./fetch_details");
+const redis = require("redis");
+const puppet = require("puppeteer");
+const { rawListeners } = require("process");
 
 const app = express();
-const port = process.env.PORT||3000
-const redis_port = 6379
+const port = process.env.PORT || 3000;
+const redis_port = 6379;
 
-const client = redis.createClient(6379,"redis");
+const client = redis.createClient(redis_port, "redis");
 
-const url_leetcode = 'https://leetcode.com/';
-const url_codeforces = 'https://codeforces.com/profile/';
-const url_codechef = 'https://www.codechef.com/users/';
+const url_leetcode = "https://leetcode.com/";
+const url_codeforces = "https://codeforces.com/profile/";
+const url_codechef = "https://www.codechef.com/users/";
 
+let browser = null;
+puppet
+  .launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+    ],
+  })
+  .then((brw) => {
+    browser = brw;
+  });
 
 app.use(function (req, res, next) {
-    let key = '__express__'+req.query.cdchf+
-    req.query.cdf+req.query.leet;
-    client.get(key,(err,data)=>{
-        if(err){
-            console.log(err);
-            next()
-        }
-        if(data!=null){
-            res.send(JSON.parse(data));
-        }
-        else{
-            res.sendResponse = res.send
-            res.send = (body) =>{
-               
-                client.setex(key,600,JSON.stringify(body));
-                res.sendResponse(body)
-            }
-            next()
-        }
-    })
-    
-  })
+  let key = "__express__" + req.query.cdchf + req.query.cdf + req.query.leet;
+  client.get(key, (err, data) => {
+    if (err) {
+      console.log(err);
+      next();
+    }
+    if (data != null) {
+      res.send(JSON.parse(data));
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        client.setex(key, 600, JSON.stringify(body));
+        res.sendResponse(body);
+      };
+      next();
+    }
+  });
+});
 
-app.get('/', (req, res) => {
-    let cdchf = req.query.cdchf;
-    let cdf = req.query.cdf;
-    let leet = req.query.leet;
-    fetch_details(url_codechef+cdchf,url_leetcode+leet,url_codeforces+cdf).then((details)=>{
-        res.send(details);
+app.get("/", (req, res) => {
+  let cdchf = req.query.cdchf;
+  let cdf = req.query.cdf;
+  let leet = req.query.leet;
+  fetch_details(
+    url_codechef + cdchf,
+    url_leetcode + leet,
+    url_codeforces + cdf,
+    browser
+  ).then((details) => {
+    res.send(details);
+  });
+});
 
-    })
-
-})
-
-
-
-    app.listen(port, () => {
-        console.log(`app listening at ${process.pid} http://localhost:${port}`)
-    })
+app.listen(port, () => {
+  console.log(`app listening at ${process.pid} http://localhost:${port}`);
+});
--- fetch_details.js
+++ fetch_details.js
@@ -1,117 +1,92 @@
-const puppet = require('puppeteer')
-const ch = require('cheerio');
+const ch = require("cheerio");
 
+async function rqt(url, callback, selector, browser) {
+  const page = await browser.newPage();
+  try {
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (request.resourceType() === "image") request.abort();
+      else request.continue();
+    });
 
-
-
-async function rqt(url,callback,selector,browser){
-  
-
-    try{
-       
-      
-       const page = await browser.newPage();
-       await page.setRequestInterception(true)
-       page.on('request', (request) => {
-       if (request.resourceType() === 'image') request.abort()
-       else request.continue()
-         })
-       
-       await page.goto(url)
-      let selectorExists = await page.waitForSelector(selector)
-       const html = await page.content();
-       const $ = ch.load(html);
-   
-       return callback($)
-  
-    }
-    catch(err){
-        console.log(err);
-        return null;
-    }
-
-}
-
-
-function check(value){
-  return new Promise((resolve,reject)=>{
-    if(value==3){
-      resolve('');
-    }
-  })
-}
-
-
-async function fetch_details(url_codechef,url_leetcode,url_codeforces){
-  const browser = await puppet.launch({
-    headless: true,
-    args: ['--no-sandbox','--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-accelerated-2d-canvas',
-    '--no-first-run',
-    '--no-zygote']
-  });
-
-    try{
-    var result = {};
-    result['codechef'] = [];
-    [value3,value2,value] = await Promise.all([
-    rqt(url_leetcode,
-    callback_leetcode,'.total-solved-count__2El1',browser),
-    
-    
-   
-    rqt(url_codechef,($)=>{
-      return $('.content').children('h5').map(function() {
-          var num = $(this).text().toString().split("(")[1];
-          num = num.slice(0,num.length-1);
-          result['codechef'].push(num);
-        });
-    },'.content',browser),
-    
-    rqt(url_codeforces,
-    callback_codeforces,'._UserActivityFrame_counterValue',browser)
-  ]
-    )
-    if(value3!=null){
-    result['leetcode'] = value3.toString()
-    }
-    else{
-      result['leetcode'] = '0';
-    }
-    if(value!=null){
-    value = value.toString();
-    result['codeforces'] = value.split(" ")[0];
-    }
-    else{
-      result['codeforces'] = '0';
-    }
-  
-    return result;
-}
-catch(err){
+    await page.goto(url);
+    let selectorExists = await page.waitForSelector(selector);
+    const html = await page.content();
+    const $ = ch.load(html);
+    return callback($);
+  } catch (err) {
     console.log(err);
-}
-finally{
-  await browser.close();
-}
-
-    
+    return null;
+  } finally {
+    await page.close();
+  }
 }
 
+async function fetch_details(
+  url_codechef,
+  url_leetcode,
+  url_codeforces,
+  browser
+) {
+  try {
+    var result = {};
+    result["codechef"] = [];
+    [value3, value2, value] = await Promise.all([
+      rqt(
+        url_leetcode,
+        callback_leetcode,
+        ".total-solved-count__2El1",
+        browser
+      ),
 
-const callback_codeforces = ($)=>{
-  return $('._UserActivityFrame_counterValue').text();
+      rqt(
+        url_codechef,
+        ($) => {
+          return $(".content")
+            .children("h5")
+            .map(function () {
+              var num = $(this).text().toString().split("(")[1];
+              num = num.slice(0, num.length - 1);
+              result["codechef"].push(num);
+            });
+        },
+        ".content",
+        browser
+      ),
+
+      rqt(
+        url_codeforces,
+        callback_codeforces,
+        "._UserActivityFrame_counterValue",
+        browser
+      ),
+    ]);
+    if (value3 != null) {
+      result["leetcode"] = value3.toString();
+    } else {
+      result["leetcode"] = "0";
+    }
+    if (value != null) {
+      value = value.toString();
+      result["codeforces"] = value.split(" ")[0];
+    } else {
+      result["codeforces"] = "0";
+    }
+
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-// const callback_codechef = 
+const callback_codeforces = ($) => {
+  return $("._UserActivityFrame_counterValue").text();
+};
 
-const callback_leetcode = ($)=>{
-  return $('.total-solved-count__2El1').text();
-}
+// const callback_codechef =
 
-module.exports = fetch_details
+const callback_leetcode = ($) => {
+  return $(".total-solved-count__2El1").text();
+};
 
-
-
-
+module.exports = fetch_details;
